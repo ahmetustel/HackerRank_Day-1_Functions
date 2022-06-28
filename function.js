7c5a024ae9f2d299733837d301b354986451acea
Ahmet Ustel <ustelahmett@gmail.com>
2022-06-28 14:07:48 +0300

    güncelleme

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 function factorial() {
-  let sayi = document.getElementById("deger").value;
-  sayi = Number(sayi);
-  sayi = parseInt(sayi);
+  let sayi = document.getElementById("deger").value; //İnput değerin değişkene atılması
+  sayi = Number(sayi); // input'un number'a dönüştürülmesi
+  sayi = parseInt(sayi); //Tamsayıya dönüştürlmesi
   let sonuc = 1;
   while (1 <= sayi && sayi <= 10) {
     for (let i = 1; i <= sayi; i++) {
